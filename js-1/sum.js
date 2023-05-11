78c14cf973f099da4cd97ac9e4a8201f102fad85
dasan <david.felipe.sanchez2@gmail.com>
2023-05-11 16:43:16 -0500

    solucion a los ejercicios de js

---
+++ js-1/sum.js
@@ -0,0 +1,8 @@
+function sum(n){
+    return ((n + 1) * n)/2  ;
+}// escribe la función suma acá
+
+// código de prueba
+console.log(sum(4)) // 1 + 2 + 3 + 4 = 10
+console.log(sum(10)) // 55
+console.log(sum(15)) // 120
